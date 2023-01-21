774df492998896556decea43893cd5e47c4eabce
iamtonmoy0 <tonmoy2code@gmail.com>
2023-01-21 10:06:22 +0600

    struct and scraper func added

--- main.go
+++ main.go
@@ -1,5 +1,31 @@
 package main
 
-func main() {
+import (
+	"crypto/rand"
+	"fmt"
+)
 
+var googleDomains = map[string]string{}
+
+type SearchResult struct {
+	ResultRank  int
+	ResultURL   string
+	ResultTitle string
+	ResultDesc  string
+}
+
+var userAgents = []string{}
+
+func randomUserAgent() string {
+	randNum := rand.Int() % len(userAgents)
+	return userAgents[randNum]
+
+}
+func main() {
+	res, err := GoogleScrape("akhil sharma")
+	if err == nil {
+		for _, res := range res {
+			fmt.Println(res)
+		}
+	}
 }
