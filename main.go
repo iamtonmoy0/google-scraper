053e4078ac94150f3d2aaa225db1fe55844a721c
iamtonmoy0 <tonmoy2code@gmail.com>
2023-01-21 12:50:07 +0600

    scraper func added

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"crypto/rand"
 	"fmt"
 	"strings"
+
+	"golang.org/x/tools/go/analysis/passes/nilfunc"
 )
 
 var googleDomains = map[string]string{}
@@ -22,22 +24,35 @@ func randomUserAgent() string {
 	return userAgents[randNum]
 
 }
-func buildGoogleUrls(searchTerm, countryCode) {
+func buildGoogleUrls(searchTerm, countryCode,pages,count int) {
 
 	toScrape := []string{}
 	searchTerm = strings.Trim(searchTerm, "")
 	searchTerm = strings.Replace(searchTerm, "", "+", -1)
+	if googleBase,found:=googleDomains[countryCode];found{
+		for i :=0,i<pages;i++{
+			start:=i*count
+			scrapeURL:= fmt.Sprints("%s%s&num=%d&hl=%s&start=%d&filter=0"googleBase,searchTerm,count,languageCode ,start)
+		}
+	}else{
+		err:=fmt.Error("country (%s) is currently not supported",countryCode)
+		return nil,err
+	}
+	return toScrape , nil
 
 }
-func GoogleScrape() ([]SearchResult, err) {
+func GoogleScrape(searchTerm,countryCode,languageCode string,pages) ([]SearchResult, err) {
 	results := []SearchResult{}
 	resultCounter := 0
-	googlePages, err := buildGoogleUrls(searchTerm, countryCode)
+	googlePages, err := buildGoogleUrls(searchTerm, countryCode,languageCode,pages,count)
+	if err!=nil{
+		return nil,err
+	}
 
 }
 
 func main() {
-	res, err := GoogleScrape("chat gpt", "com")
+	res, err := GoogleScrape("chat gpt", "en","com",1,30)
 	if err == nil {
 		for _, res := range res {
 			fmt.Println(res)
